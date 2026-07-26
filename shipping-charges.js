ffb57e241697baa6dbe94652f022a014a870a8a5
Mr Taiwo <taiwoomosehin6@gmail.com>
2026-07-26 13:14:30 -0700

    lga-proceed-checkout

---
+++ shipping-charges.js
@@ -0,0 +1,159 @@
+const accessoryLGACharges = {
+    "Agege LGA": 6000,
+    "Orile-Agege LCDA": 7000,
+
+    "Ajeromi-Ifelodun LGA": 9000,
+    "Ifelodun LCDA": 10000,
+
+    "Alimosho LGA": 10000,
+    "Ayobo-Ipaja LCDA": 8000,
+    "Egbe-Idimu LCDA": 9000,
+    "Igando-Ikotun LCDA": 10000,
+    "Mosan Okunola LCDA": 11500,
+
+    "Amuwo-Odofin LGA": 10990,
+    "Oriade LCDA": 8500,
+
+    "Apapa LGA": 9000,
+    "Apapa-Iganmu LCDA": 8200,
+
+    "Badagry LGA": 15600,
+    "Badagry West LCDA": 15000,
+    "Olorunda LCDA": 16500,
+
+    "Epe LGA": 13750,
+    "Eredo LCDA": 15000,
+    "Ikosi-Ejinrin LCDA": 16000,
+
+    "Eti-Osa LGA": 11000,
+    "Eti-Osa East LCDA": 12900,
+    "Iru Victoria Island LCDA": 10500,
+    "Ikoyi Obalende LCDA": 10200,
+
+    "Ibeju-Lekki LGA": 10990,
+    "Lekki LCDA": 11200,
+
+    "Ifako-Ijaiye LGA": 9400,
+    "Ojokoro LCDA": 10900,
+    "Agbado/Oke-Odo LCDA": 10300,
+
+    "Ikeja LGA": 8600,
+    "Ojodu LCDA": 8000,
+    "Onigbongbo LCDA": 7400,
+
+    "Ikorodu LGA": 10000,
+    "Ikorodu North LCDA": 10000,
+    "Ikorodu West LCDA": 11000,
+    "Imota LCDA": 12000,
+    "Ijede LCDA": 10100,
+    "Igbogbo-Baiyeku LCDA": 10980,
+
+    "Kosofe LGA": 6000,
+    "Ikosi-Isheri LCDA": 6000,
+    "Agboyi-Ketu LCDA": 6000,
+
+    "Lagos Island LGA": 5000,
+    "Lagos Island East LCDA": 5000,
+
+    "Lagos Mainland LGA": 4000,
+    "Yaba LCDA": 4500,
+
+    "Mushin LGA": 3200,
+    "Odi-Olowo/Ojuwoye LCDA": 3000,
+
+    "Ojo LGA": 7000,
+    "Oto-Awori LCDA": 6600,
+    "Iba LCDA": 6500,
+
+    "Somolu LGA": 5000,
+    "Bariga LCDA": 5000,
+
+    "Oshodi-Isolo LGA": 3000,
+    "Isolo LCDA": 3500,
+    "Ejigbo LCDA": 3590,
+
+    "Surulere LGA": 4500,
+    "Coker-Aguda LCDA": 5000,
+    "Itire-Ikate LCDA": 5000
+};
+
+// Machine LGA Charges
+
+const machineLGACharges = {
+    "Agege LGA": 25000,
+    "Orile-Agege LCDA": 25000,
+
+    "Ajeromi-Ifelodun LGA": 25000,
+    "Ifelodun LCDA": 25000,
+
+    "Alimosho LGA": 27000,
+    "Ayobo-Ipaja LCDA": 31000,
+    "Egbe-Idimu LCDA": 26000,
+    "Igando-Ikotun LCDA": 29000,
+    "Mosan Okunola LCDA": 31500,
+
+    "Amuwo-Odofin LGA": 26990,
+    "Oriade LCDA": 28500,
+
+    "Apapa LGA": 29000,
+    "Apapa-Iganmu LCDA": 25200,
+
+    "Badagry LGA": 33600,
+    "Badagry West LCDA": 35000,
+    "Olorunda LCDA": 36500,
+
+    "Epe LGA": 38750,
+    "Eredo LCDA": 39000,
+    "Ikosi-Ejinrin LCDA": 37000,
+
+    "Eti-Osa LGA": 31000,
+    "Eti-Osa East LCDA": 32900,
+    "Iru Victoria Island LCDA": 29500,
+    "Ikoyi Obalende LCDA": 28200,
+
+    "Ibeju-Lekki LGA": 39990,
+    "Lekki LCDA": 36200,
+
+    "Ifako-Ijaiye LGA": 28400,
+    "Ojokoro LCDA": 30900,
+    "Agbado/Oke-Odo LCDA": 32300,
+
+    "Ikeja LGA": 29600,
+    "Ojodu LCDA": 32000,
+    "Onigbongbo LCDA": 27400,
+
+    "Ikorodu LGA": 30000,
+    "Ikorodu North LCDA": 31000,
+    "Ikorodu West LCDA": 33000,
+    "Imota LCDA": 33900,
+    "Ijede LCDA": 34100,
+    "Igbogbo-Baiyeku LCDA": 34980,
+
+    "Kosofe LGA": 29000,
+    "Ikosi-Isheri LCDA": 31000,
+    "Agboyi-Ketu LCDA": 31800,
+
+    "Lagos Island LGA": 30000,
+    "Lagos Island East LCDA": 32000,
+
+    "Lagos Mainland LGA": 19000,
+    "Yaba LCDA": 15000,
+
+    "Mushin LGA": 14200,
+    "Odi-Olowo/Ojuwoye LCDA": 16000,
+
+    "Ojo LGA": 31009,
+    "Oto-Awori LCDA": 31600,
+    "Iba LCDA": 32500,
+
+    "Somolu LGA": 19000,
+    "Bariga LCDA": 21000,
+
+    "Oshodi-Isolo LGA": 13000,
+    "Isolo LCDA": 23500,
+    "Ejigbo LCDA": 24000,
+
+    "Surulere LGA": 20000,
+    "Coker-Aguda LCDA": 22000,
+    "Itire-Ikate LCDA": 24500
+};
